00f7e779d433342f979174f12b0c08e2707e77f6
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-25 12:55:14 -0400

    Llenar la tabla de países con los valores que trae la API y *ngFor. Mostrar la tabla sólo cuando la API devuelve paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
 
+import { Country } from '../../interfaces/pais.interface';
+
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
@@ -11,6 +13,7 @@ export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor( private paisService: PaisService ) {}
 
@@ -22,9 +25,11 @@ export class PorPaisComponent {
       .subscribe({
         next: (paises) => {
           console.log( paises );
+          this.paises = paises;
         },
         error: (err) => {
           this.hayError = true;
+          this.paises   = [];
         }
       })
         
