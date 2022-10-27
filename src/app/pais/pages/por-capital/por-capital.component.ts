3cc0b886ea5278d8caf2436d7862d728767fb09e
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-27 17:45:10 -0400

    Corrigiendo error de las sugerencias en la búsqueda por capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -24,6 +24,7 @@ export class PorCapitalComponent {
 
     this.hayError = false;
     this.termino  = termino;
+    this.mostrarSugerencias = false;
 
     this.paisService.buscarCapital( termino )
       .subscribe({
