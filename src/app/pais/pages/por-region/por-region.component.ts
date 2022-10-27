575fb40e108c0e5fd5fb48058abff3a33304c933
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-27 10:38:58 -0400

    Mostrar países por región con la API de REST Countries

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
@@ -13,18 +16,24 @@ export class PorRegionComponent {
 
   regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
   regionActiva: string = '';
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor( private paisService: PaisService ) { }
 
   getClaseCSS( region: string ): string {
     return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
 
   activarRegion( region: string ){
+
+    if ( region === this.regionActiva ) return; 
     
     this.regionActiva = region;
+    this.paises = [];
 
     //TODO: hacer el llamado al servicio
+    this.paisService.buscarRegion( region )
+      .subscribe( paises => this.paises = paises )
   }
 
 }
